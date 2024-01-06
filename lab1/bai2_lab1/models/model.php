2b4f42b81068d7462d499570f21cd034a89c9777
nguyenhuuphat1512 <nguyenhuuphat2812@gmail.com>
2024-01-06 15:28:30 +0700

    bai2

---
+++ lab1/bai2_lab1/models/model.php
@@ -0,0 +1,10 @@
+<?php
+//models
+function get_courses(){
+    include 'data.php';
+    return array_values($course); 
+}
+function find_by_semester($semester){
+    include 'data.php';
+    return (array_key_exists($semes, $course) ? $course [$semester] : 'Invalid course'); 
+}
